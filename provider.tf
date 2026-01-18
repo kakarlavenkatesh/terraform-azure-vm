64fa8814095d62e0fd32d72481a0ce948fa3b870
kakarlavenkatesh <kakarlavenkatesh@gmail.com>
2026-01-18 16:16:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,3 +12,9 @@ terraform {
 provider "azurerm" {
   features {}
 }
+backend "azurerm" {
+  resource_group_name  = "tfstate-day04"
+  storage_account_name = "day0416658"
+  container_name       = "tfstate"
+  key                  = "dev.terraform.tfstate"
+}
