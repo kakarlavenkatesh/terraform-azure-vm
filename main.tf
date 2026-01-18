7d8157a7f2ccef609f0d329aa5fd877518799347
kakarlavenkatesh <kakarlavenkatesh@gmail.com>
2026-01-18 13:38:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,6 +40,14 @@ resource "azurerm_linux_virtual_machine" "vm" {
     azurerm_network_interface.nic.id
   ]
 
+  # 🔐 SSH authentication (REQUIRED)
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCLG8aHTd6sP0XTcBs9uJFZNV/1Kb1EeedYtxZSenuFIsa8fzow8srOzv3xO3CTAoGcDHhbhWEMJTlvsmnzYioNoywuUlXTn+m1VWlvJznWKK76bXN9MedfydoIED9n2tjiaAWTKOSYDYU3CjiAHJhN4U4yJONNaFBMxaliEWq/uqKQHGHhdWIHepJNH/tux0c+87jOV957QkmBuGhGWJ/OqH6AdujVKJcMIfFlmwypNq5Rte8v32SEjpf3oQMnIdOOoUJ8kPBESkAijdmOCWSV+uMBePAjzQx+P5ZIU8XgXFGge2M3AptsTtxoSs/UdS71XakrbNC9k98if6YVAkGeg08dokuOMPZCyrsZH8eUkA6D9JRcKZbZNYf5+pfQyTygzksXNdgzeN5JAzL5UfCkGCGP+VAMwzutsKIVqfx3Kje57jQKv9zSIVMxE/AShQttlFhwayk9GtecctJ6Ceg7ftYNqkZDpd84rxPhpZx9gUveZ1EWGRmHnsaSNg/jVQHo7d80EpDsu1HzR+Fnlwuhw4HsVb6JAxF0y8XLv1PtO9hE+HPqd9K+cU5Fdw8titBI2llU5dZ9cctpEzR5xr8twkKMb5isxD49T47Zo3e2wCubQwCnb43cnBPLEW3Wi287NL4EHuTVHDwZ/OIztCAOYHyH3sOFtG1H2PIGOl8M7Q== azureuser@linnux-jenkins"
+  }
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
