16f3acabce4c3834fa5ba72302052883bdfe719a
kakarlavenkatesh <kakarlavenkatesh@gmail.com>
2026-01-18 14:41:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,19 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 4.57.0"
     }
-  }
-  backend "azurerm" {
-    resource_group_name  = "tfstate-day04"
-    storage_account_name = "day0416658"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
 }
-
-# ... (rest of your VM resources)
